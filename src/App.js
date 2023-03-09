979c5f9600d3e0fb9a014d98358d72f262bbe894
Sarup Das <neeopixel@gmail.com>
2023-03-09 12:46:39 +0530

    Commit #3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './images/logo.svg';
-import './App.css';
-import Draggable from 'react-draggable';
-
-
+import logo from "./images/logo.svg";
+import "./App.css";
+import Draggable from "react-draggable";
+import { router, routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Draggable bounds="parent">
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="appparent">
+            <router>
+              <routes> </routes>
+              <routes> </routes>
+              <routes> </routes>
+            </router>
+            <img src={logo} className="App-logo" alt="logo" />;
+          </div>
         </Draggable>
       </header>
     </div>
